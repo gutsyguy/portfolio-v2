f7518c796d26e328149fdb1bb198ef69b0061317
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-12-16 00:16:28 -0800

    Finished implementing color changes

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -35,9 +35,9 @@ const Timeline = () => {
       </div>
       {/* ... */}
       <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
-        <div className="py-3 sm:max-w-xl sm:mx-auto w-full px-2 sm:px-0">
-          <div className="relative text-gray-700 antialiased text-sm font-semibold">
-            <div className="hidden sm:block w-1 bg-red-400 absolute h-full left-1/2 transform -translate-x-1/2"></div>
+        <div className="py-3  sm:max-w-xl sm:mx-auto w-full px-2 sm:px-0">
+          <div className="relative  text-gray-700 antialiased text-sm font-semibold">
+            <div className="hidden border border-gray-800 sm:block w-1 bg-red-400 absolute h-full left-1/2 transform -translate-x-1/2"></div>
             {fakeTimeline.map((timelineElement, i) =>
               checkTimelineSide(i) ? (
                 <LeftTimelineComponent
